6a438529cf7de7b63020f23c8a49f32c3aa4e1ad
phuongnguyen1399 <phuongnvx9399@gmail.com>
2026-08-09 14:43:19 +0700

    Leetcode 271: Encode and Decode Strings

--- src/dsa_java/neetcode/array_and_hashing/EncodeandDecodeStrings.java
+++ src/dsa_java/neetcode/array_and_hashing/EncodeandDecodeStrings.java
@@ -1,19 +1,49 @@
 package dsa_java.neetcode.array_and_hashing;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class EncodeandDecodeStrings {
     public static void main(String[] args) {
-        
+        List<String> input = List.of("");
+        // String output = List.of("Phuong", "Nguyen");
+        System.out.println(encode(input));
+        System.out.println("==============================");
+        System.out.println(decode(encode(input)));
     }
 
     public static String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
 
-        return "";
+        for (String str : strs) {
+            sb.append(str.length()).append("#").append(str);
+        }
+
+        return sb.toString();
     }
 
-    public static List<String> decode(String str) {
+    public static List<String> decode(String str) { //0#0#
         
-        return null;
+        List<String> result = new ArrayList<>();
+        
+        int i = 0;
+
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            int len = Integer.parseInt(str.substring(i, j));
+            if (len != 0) {
+                result.add(str.substring(j + 1, j + 1 + len));
+                i = j + 1 + len;
+            } else {
+                result.add("");
+                i += 2;
+            }
+        }
+
+        return result;
     }
 }
