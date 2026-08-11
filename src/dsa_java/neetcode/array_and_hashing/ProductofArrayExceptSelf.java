7db9f8777712aba2e0d4549a483b735302ab0335
phuongnguyen1399 <phuongnvx9399@gmail.com>
2026-08-11 22:18:55 +0700

    Leetcode 238: Products of Array Except Self. Solution 2: O(n) time complexity and O(1) space complexity

--- src/dsa_java/neetcode/array_and_hashing/ProductofArrayExceptSelf.java
+++ src/dsa_java/neetcode/array_and_hashing/ProductofArrayExceptSelf.java
@@ -3,7 +3,7 @@ package dsa_java.neetcode.array_and_hashing;
 public class ProductofArrayExceptSelf {
     public static void main(String[] args) {
         int[] nums = { 1,2,3,4 }; 
-        int[] rs = productExceptSelf1(nums);
+        int[] rs = productExceptSelf2(nums);
         for (int i : rs) {
             System.out.print(i + " - ");
         }
@@ -33,6 +33,29 @@ public class ProductofArrayExceptSelf {
         int[] result = new int[nums.length];
         for (int k = 0; k < result.length; k++) {
             result[k] = prefixProduct[k] * suffixProduct[k];
+        }
+
+        return result;
+    }
+
+    //Solution 2: O(n) time complexity and O(1) space complexity
+    public static int[] productExceptSelf2(int[] nums) {
+        int len = nums.length;
+
+        //Create result array which is also a prefix product array
+        int[] result = new int[len];
+        result[0] = 1;
+
+        //First loop to create prefix product
+        for (int i = 1; i < len; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        //Second loop with suffix temp variable to calculate result //{ 1,1,2,6 }; 
+        int suffTmp = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            result[i] = suffTmp * result[i];
+            suffTmp = suffTmp * nums[i];
         }
 
         return result;
